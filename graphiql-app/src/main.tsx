1b1636c36a8bb410bd817c24cf34f3ae801f7b80
avshir <av.shirinskaya@gmail.com>
2023-05-13 12:55:11 +0200

    feat: add firebase

--- graphiql-app/src/main.tsx
+++ graphiql-app/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import 'bootswatch/dist/vapor/bootstrap.min.css';
 import './index.scss';
+
 import App from './app/App';
 import store from './app/store';
+import './firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
