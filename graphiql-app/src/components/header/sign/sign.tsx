d72cebe94bdd16b0139ccb8d54179e74188dc6b6
avshir <av.shirinskaya@gmail.com>
2023-05-13 13:04:12 +0200

    feat: update header and sign components

--- graphiql-app/src/components/header/sign/sign.tsx
+++ graphiql-app/src/components/header/sign/sign.tsx
@@ -1,5 +1,17 @@
 import './sign-style.scss';
+import { useTranslation } from 'react-i18next';
 
-export default function Sign() {
-  return <div className={'header-sign'}>Sign</div>;
+interface ISignProps {
+  handleClick: () => void;
+}
+
+export default function Sign({ handleClick }: ISignProps) {
+  return (
+    <div className={'header-sign'}>
+      <button type="button" className="btn btn-secondary" onClick={handleClick}>
+        sign out
+        {/* {t('header.sign')} */}
+      </button>
+    </div>
+  );
 }
