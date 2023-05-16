60d7018bdd7875d4219d4255cd7bbf0d5a571fcb
tatiana <miss.lacrima.ts@gmail.com>
2023-05-16 18:06:47 +0300

    refactor: replace array with imported const variable

--- graphiql-app/src/app/App.tsx
+++ graphiql-app/src/app/App.tsx
@@ -8,10 +8,11 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import LoginPage from '../routes/login-page';
 import RegisterPage from '../routes/register-page';
+import { OFFSET_Y_SCROLL } from '../utils/animation-data';
 
 export default function App() {
   const { scrollY } = useScroll();
-  const offsetY = [0, 500];
+  const offsetY = OFFSET_Y_SCROLL;
 
   return (
     <div className="wrapper">
