fa35effcc35eb5e8686bb7d219ce7e6d581dcc0b
tatiana <miss.lacrima.ts@gmail.com>
2023-05-16 18:13:23 +0300

    feat: add new components and animation to welcome page

--- graphiql-app/src/routes/welcome-page/welcome-page.tsx
+++ graphiql-app/src/routes/welcome-page/welcome-page.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 import { useAuth } from '../../hooks/use-auth';
+import Project from '../../components/content/welcome-page/project';
+import Course from '../../components/content/welcome-page/course';
+import Cards from '../../components/cards/welcome-page';
+import { OFFSET_Y_SCROLL } from '../../utils/animation-data';
 
 import './welcome-page.scss';
 
 const WelcomePage = () => {
+  const { scrollY } = useScroll();
+  const offsetY = OFFSET_Y_SCROLL;
+  const marginTop = useTransform(scrollY, offsetY, offsetY);
+
   const { isAuth } = useAuth();
 
   const content = isAuth ? (
@@ -24,10 +32,12 @@ const WelcomePage = () => {
   );
 
   return (
-    <div className="welcome-page">
+    <motion.main className="welcome-page" style={{ marginTop }}>
       <div className="btn__container">{content}</div>
-      <h1 className="text-center mb-4">Welcome Page</h1>
-    </div>
+      <Project></Project>
+      <Cards></Cards>
+      <Course></Course>
+    </motion.main>
   );
 };
 
