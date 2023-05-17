f5b695a4c30d43568e6d1bee4a5bc3153f8c409e
avshir <av.shirinskaya@gmail.com>
2023-05-17 18:20:24 +0200

    feat: add localization and update styles for login-page and register-page components

--- graphiql-app/src/components/authorization/login/login.tsx
+++ graphiql-app/src/components/authorization/login/login.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useTranslation } from 'react-i18next';
 
 import AuthForm from '../auth-form/auth-form';
 import { useAppDispatch } from './../../../utils/hooks';
 import { setUser } from '../../../features/slices/userSlice';
+import ErrorMessage from '../error-message';
 
 const Login = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  const [error, setError] = useState(false);
+  const [typeError, setTypeError] = useState('');
 
   const handlerLogin = (email: string, password: string) => {
     const auth = getAuth();
+    setError(false);
+
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
@@ -25,13 +33,34 @@ const Login = () => {
         navigate('/main');
       })
       .catch((error) => {
-        console.error;
-        console.log(error.code, error.message);
-        alert(`User with email:${email} is not found. Try again!`);
+        console.log(error.code);
+        setError(true);
+
+        if (error.code === 'auth/wrong-password') {
+          setTypeError('wrongPassword');
+        } else if (error.code === 'auth/user-not-found') {
+          setTypeError('userNotFound');
+        } else if (error.code === 'auth/too-many-requests') {
+          setTypeError('tooManyRequests');
+        } else {
+          setTypeError('unknownError');
+        }
       });
   };
 
-  return <AuthForm titleBtn="sign in" handleClick={handlerLogin} />;
+  const errorMessageBlock = error ? (
+    <ErrorMessage
+      errorTextMessage={t(`errorTextMessage.${typeError}`)}
+      handleClick={() => setError(false)}
+    />
+  ) : null;
+
+  return (
+    <>
+      {errorMessageBlock}
+      <AuthForm titleBtn={t('buttons.signIn')} handleClick={handlerLogin} />
+    </>
+  );
 };
 
 export default Login;
