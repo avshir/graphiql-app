db04e43f601eb05b4247f6b4a5dfabb7bea65870
tatiana <miss.lacrima.ts@gmail.com>
2023-05-18 22:36:10 +0300

    feat: save language to local storage

--- graphiql-app/src/i18n.ts
+++ graphiql-app/src/i18n.ts
@@ -1,6 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import { getDefaultLanguage } from './utils/default-language';
+
 const resources = {
   en: {
     translation: {
@@ -110,7 +112,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en', // if you're using a language detector, do not define the lng option
+  lng: getDefaultLanguage(), // if you're using a language detector, do not define the lng option
   fallbackLng: 'en',
 });
 
