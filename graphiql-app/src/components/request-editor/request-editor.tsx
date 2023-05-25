b7d10c069ab310bb14bf95c72dfde143e8915d12
github-dns147 <rarden@ya.ru>
2023-05-25 20:31:38 +0300

    refactor: request button moved to request section

--- graphiql-app/src/components/request-editor/request-editor.tsx
+++ graphiql-app/src/components/request-editor/request-editor.tsx
@@ -5,27 +5,61 @@ import { useCallback, useEffect, useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { aura } from '@uiw/codemirror-theme-aura';
+import { fetchData } from '../../features/apiSlice';
+
+interface IVariables {
+  [key: string]: string;
+}
 
 export default function RequestEditor() {
   const dispatch = useAppDispatch();
-  const query = useAppSelector((state) => state.query.value) as string;
+  const contentQuery = useAppSelector((state) => state.query.value) as string;
   const [content, setContent] = useState('');
+
+  const query: string = useAppSelector((state) => state.request.value);
+  const variables: IVariables = useAppSelector((state) => state.variables.value);
+
+  const [requestQuery, setRequestQuery] = useState('');
 
   const handleChange = useCallback((value: string) => {
     setContent(value);
   }, []);
 
   useEffect(() => {
-    setContent(query);
-  }, [query]);
+    setContent(contentQuery);
+  }, [contentQuery]);
 
   useEffect(() => {
     dispatch(setRequest(content));
   }, [dispatch, content]);
 
+  useEffect(() => {
+    const splitQuery = query.split('\n');
+    const cloneSplitQuery = splitQuery.slice();
+
+    for (const key in variables) {
+      cloneSplitQuery.forEach((elemQuery, index) => {
+        const newElemQuery =
+          elemQuery.includes(`${key}(code:`) || elemQuery.includes(`${key} (code:`)
+            ? `  ${key}(code: "${variables[key]}") {`
+            : elemQuery;
+        cloneSplitQuery[index] = newElemQuery;
+      });
+    }
+
+    const resultQuery = cloneSplitQuery.join('\n');
+
+    setRequestQuery(resultQuery);
+  }, [query, variables]);
+
+  const handleClick = async (): Promise<void> => {
+    dispatch(fetchData(requestQuery));
+  };
+
   return (
     <div className="request-editor glass-effect">
       <h5 className="header-section">Operation</h5>
+      <button className="request-btn" onClick={handleClick}></button>
       <CodeMirror
         className="request-editor-textarea"
         value={content}
--- graphiql-app/src/routes/main-page/main-page.tsx
+++ graphiql-app/src/routes/main-page/main-page.tsx
@@ -1,53 +1,14 @@
 import './main-page.scss';
-import { fetchData } from '../../features/apiSlice';
-import { useAppDispatch, useAppSelector } from '../../utils/hooks';
 import RequestEditor from '../../components/request-editor';
 import DocumentationExplorer from '../../components/documentation-explorer';
 import ResponseSection from '../../components/response-section';
 import VariablesEditor from '../../components/variables-editor/variables-editor';
-import { useEffect, useState } from 'react';
 import { Resizable } from 're-resizable';
 
-interface IVariables {
-  [key: string]: string;
-}
-
 export default function MainPage() {
-  const dispatch = useAppDispatch();
-  const query: string = useAppSelector((state) => state.request.value);
-  const variables: IVariables = useAppSelector((state) => state.variables.value);
-
-  const [requestQuery, setRequestQuery] = useState('');
-
-  useEffect(() => {
-    const splitQuery = query.split('\n');
-    const cloneSplitQuery = splitQuery.slice();
-
-    for (const key in variables) {
-      cloneSplitQuery.forEach((elemQuery, index) => {
-        const newElemQuery =
-          elemQuery.includes(`${key}(code:`) || elemQuery.includes(`${key} (code:`)
-            ? `  ${key}(code: "${variables[key]}") {`
-            : elemQuery;
-        cloneSplitQuery[index] = newElemQuery;
-      });
-    }
-
-    const resultQuery = cloneSplitQuery.join('\n');
-
-    setRequestQuery(resultQuery);
-  }, [query, variables]);
-
-  const handleClick = async (): Promise<void> => {
-    dispatch(fetchData(requestQuery));
-  };
-
   return (
     <>
-      <main className="main-graphql-container main-page container">
-        <button className="request-btn btn btn-secondary" onClick={handleClick}>
-          Request
-        </button>
+      <div className="main-graphql-container container">
         <div className="graphql-container">
           <Resizable
             className="doc-query"
@@ -114,7 +75,7 @@ export default function MainPage() {
             <ResponseSection />
           </Resizable>
         </div>
-      </main>
+      </div>
     </>
   );
 }
