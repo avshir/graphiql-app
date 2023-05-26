bc2e80544434d05b243969d7594b1cc3553859d1
tatiana <miss.lacrima.ts@gmail.com>
2023-05-26 22:07:13 +0300

    feat: put modal inside main tag and wrap outlet with error boundary

--- graphiql-app/src/features/layout/layout.tsx
+++ graphiql-app/src/features/layout/layout.tsx
@@ -1,20 +1,36 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useScroll } from 'framer-motion';
+import { ErrorBoundary } from 'react-error-boundary';
 
+import { useAppSelector } from '../../utils/hooks';
 import Footer from '../../components/footer';
 import Header from '../../components/header';
-import { useScroll } from 'framer-motion';
+import AlertMessage from '../../components/error';
+import Modal from '../../components/modal';
+
 import { OFFSET_Y_SCROLL } from '../../utils/animation-data';
 
 const Layout: FC = () => {
   const { scrollY } = useScroll();
   const offsetY = OFFSET_Y_SCROLL;
+  const { error } = useAppSelector((state) => state.data);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (error) {
+      setModalOpen(true);
+    }
+  }, [error]);
 
   return (
     <div className="wrapper">
       <Header scrollY={scrollY} offsetY={offsetY} />
       <main className="main">
-        <Outlet />
+        <ErrorBoundary FallbackComponent={AlertMessage}>
+          <Outlet />
+        </ErrorBoundary>
+        {isModalOpen && <Modal setModalOpen={setModalOpen} />}
       </main>
       <Footer />
     </div>
