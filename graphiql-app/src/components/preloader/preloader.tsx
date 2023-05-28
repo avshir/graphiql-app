ecf208e646c0208576906b16b55bbc05e4096098
tatiana <miss.lacrima.ts@gmail.com>
2023-05-28 18:12:19 +0300

    feat: add preloader component

---
+++ graphiql-app/src/components/preloader/preloader.tsx
@@ -0,0 +1,22 @@
+import './preloader-style.scss';
+
+export default function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="preloader-inner">
+        <div>
+          <div>
+            <div>
+              <div></div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
