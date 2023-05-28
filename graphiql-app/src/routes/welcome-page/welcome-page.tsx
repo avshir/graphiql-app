d7d6494315ab5d9eb88da1c7192c1416965e3e2f
tatiana <miss.lacrima.ts@gmail.com>
2023-05-28 18:10:57 +0300

    fix: change styles for welcome page container

--- graphiql-app/src/routes/welcome-page/welcome-page.tsx
+++ graphiql-app/src/routes/welcome-page/welcome-page.tsx
@@ -1,23 +1,16 @@
-import { motion, useScroll, useTransform } from 'framer-motion';
-
 import Project from '../../components/content/welcome-page/project';
 import Course from '../../components/content/welcome-page/course';
 import Cards from '../../components/cards/welcome-page';
-import { OFFSET_Y_SCROLL } from '../../utils/animation-data';
 
 import './welcome-page.scss';
 
 const WelcomePage = () => {
-  const { scrollY } = useScroll();
-  const offsetY = OFFSET_Y_SCROLL;
-  const marginTop = useTransform(scrollY, offsetY, offsetY);
-
   return (
-    <motion.div className="welcome-page" style={{ marginTop }}>
+    <div className="welcome-page">
       <Project></Project>
       <Cards></Cards>
       <Course></Course>
-    </motion.div>
+    </div>
   );
 };
 
