909f967c61c55bda1d2929ae74dc23ae80e52919
tatiana <miss.lacrima.ts@gmail.com>
2023-05-28 18:16:42 +0300

    feat: add preloader and change main tag styles

--- graphiql-app/src/features/layout/layout.tsx
+++ graphiql-app/src/features/layout/layout.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useScroll } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { useAppSelector } from '../../utils/hooks';
@@ -8,14 +8,17 @@ import Footer from '../../components/footer';
 import Header from '../../components/header';
 import AlertMessage from '../../components/error';
 import Modal from '../../components/modal';
+import Preloader from '../../components/preloader';
 
 import { OFFSET_Y_SCROLL } from '../../utils/animation-data';
 
 const Layout: FC = () => {
   const { scrollY } = useScroll();
   const offsetY = OFFSET_Y_SCROLL;
+  const marginTop = useTransform(scrollY, offsetY, offsetY);
   const { error } = useAppSelector((state) => state.data);
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     if (error) {
@@ -23,16 +26,30 @@ const Layout: FC = () => {
     }
   }, [error]);
 
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
-      <Header scrollY={scrollY} offsetY={offsetY} />
-      <main className="main">
-        <ErrorBoundary FallbackComponent={AlertMessage}>
-          <Outlet />
-        </ErrorBoundary>
-        {isModalOpen && <Modal setModalOpen={setModalOpen} />}
-      </main>
-      <Footer />
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <>
+          <Header scrollY={scrollY} offsetY={offsetY} />
+          <motion.main className="main" style={{ marginTop }}>
+            <ErrorBoundary FallbackComponent={AlertMessage}>
+              <Outlet />
+            </ErrorBoundary>
+            {isModalOpen && <Modal setModalOpen={setModalOpen} />}
+          </motion.main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
